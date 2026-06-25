9839f9ebc5c84bb093bcbc77cdc5043c64ed9478
Ahmad Alfarrel Ghazali <alfarelahmad@gmail.com>
2026-06-25 12:45:41 +0700

    refactor: adding handle failure

--- include/loading.py
+++ include/loading.py
@@ -1,10 +1,16 @@
 from include.etl_control import EtlControl
 from datetime import datetime
 from zoneinfo import ZoneInfo
+from include.logging import setup_logger, get_logger
+from include.extract import _handle_failure
 
-def load_to_snowflake(connection_provider, stage_name="nyc_taxi_stage", target_table="staging.raw_nyc_taxi_tripdata"):
-    etl_control = EtlControl(connection_provider)
 
+setup_logger()
+logger = get_logger()
+
+
+def load_to_snowflake(etl_control, stage_name="nyc_taxi_stage", target_table="staging.raw_nyc_taxi_tripdata"):
+    counter = 0
     months_to_load = etl_control.get_months_needing_load()
 
     for year_month, s3_raw_path in months_to_load:
@@ -14,18 +20,24 @@ def load_to_snowflake(connection_provider, stage_name="nyc_taxi_stage", target_t
             FILE_FORMAT = (TYPE = PARQUET)
             MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE
         """
+
         update_query = f"""
             UPDATE {target_table}
             SET year_month = %s, loaded_at = %s
             WHERE year_month IS NULL
         """
+
         try:
-            with connection_provider() as conn:
+            with etl_control._get_connection() as conn:
                 with conn.cursor() as cur:
                     cur.execute(query)
-                    print("Part 1 loading data success")
                     cur.execute(update_query, (year_month, datetime.now(tz=ZoneInfo("Asia/Jakarta"))))
-                    print(f"Loading {year_month} data success")
+
+                    logger.info(f"Loading the {year_month} to raw table success")
+
             etl_control.mark_loaded(year_month)
+
         except Exception as e:
-            etl_control.mark_failed(year_month=year_month, error_message=str(e))
+            counter = _handle_failure(
+                etl_control, year_month, str(e), counter
+            )
